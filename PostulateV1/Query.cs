f06a2870d00bc53530cbb94fb97d1bfec2748e4d
Adam O'Neil <adamosoftware@gmail.com>
2017-04-26 11:55:47 -0400

    Query class

---
+++ PostulateV1/Query.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace Postulate
+{
+    public class Query<TResult>
+    {
+        private readonly string _sql;
+        private readonly Func<IDbConnection> _connectionGetter;
+
+        public Query(string sql, Func<IDbConnection> connectionGetter)
+        {
+            _sql = sql;
+            _connectionGetter = connectionGetter;
+        }
+
+        public string Sql { get { return _sql; } }
+
+        public int CommandTimeout { get; set; } = 30;
+
+        public CommandType CommandType { get; set; } = CommandType.Text;
+
+        public IEnumerable<TResult> Execute()
+        {
+            using (IDbConnection cn = _connectionGetter.Invoke())
+            {
+                cn.Open();
+                return Execute(cn);
+            }
+        }
+
+        public IEnumerable<TResult> Execute(IDbConnection connection)
+        {
+            return connection.Query<TResult>(_sql, this, commandType: CommandType);
+        }
+
+        public async Task<IEnumerable<TResult>> ExecuteAsync()
+        {
+            using (IDbConnection cn = _connectionGetter.Invoke())
+            {
+                cn.Open();
+                return await ExecuteAsync(cn);
+            }
+        }
+
+        public async Task<IEnumerable<TResult>> ExecuteAsync(IDbConnection connection)
+        {
+            return await connection.QueryAsync<TResult>(_sql, this, commandType: CommandType);
+        }
+
+        public TResult ExecuteSingle()
+        {
+            using (IDbConnection cn = _connectionGetter.Invoke())
+            {
+                cn.Open();
+                return ExecuteSingle(cn);
+            }
+        }
+
+        public TResult ExecuteSingle(IDbConnection connection)
+        {
+            return connection.QuerySingleOrDefault<TResult>(_sql, this, commandType: CommandType);
+        }
+
+        public async Task<TResult> ExecuteSingleAsync()
+        {
+            using (IDbConnection cn = _connectionGetter.Invoke())
+            {
+                cn.Open();
+                return await ExecuteSingleAsync(cn);
+            }
+        }
+
+        public async Task<TResult> ExecuteSingleAsync(IDbConnection connection)
+        {
+            return await connection.QuerySingleOrDefaultAsync<TResult>(_sql, this, commandType: CommandType);
+        }
+    }
+}
---
+++ Testing/Queries.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Postulate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testing.Models;
+
+namespace Testing
+{
+    [TestClass]
+    public class Queries
+    {
+        [TestMethod]
+        public void AnyOrgs()
+        {
+            var allOrgs = new AllOrgs().Execute();
+            Assert.IsTrue(allOrgs.Any());
+        }
+
+        [TestMethod]
+        public void OrgsWithName()
+        {
+            var orgs = new OrgsWithName("sample").Execute();
+            Assert.IsTrue(orgs.Any());
+        }
+    }
+
+    public class PostulateQuery<TResult> : Query<TResult>
+    {
+        public PostulateQuery(string sql) : base(sql, () => { return new PostulateDb().GetConnection(); })
+        {
+        }
+    }
+
+    public class AllOrgs : PostulateQuery<Organization>
+    {
+        public AllOrgs() : base("SELECT * FROM [dbo].[Organization]")
+        {
+        }
+    }
+
+    public class OrgsWithName : PostulateQuery<Organization>
+    {
+        public OrgsWithName(string name) : base("SELECT * FROM [dbo].[Organization] WHERE [Name] LIKE '%' + @name + '%'")
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }        
+    }
+}
