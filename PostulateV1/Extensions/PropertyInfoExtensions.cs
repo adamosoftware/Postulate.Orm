14bc429d84e54e4b63c62d13b3d99052448b8b56
Adam O'Neil <adamosoftware@gmail.com>
2017-05-13 12:20:14 -0400

    version 0.9.1, factored Query class to Postulate.Sql project

--- PostulateV1/Extensions/PropertyInfoExtensions.cs
+++ PostulateV1/Extensions/PropertyInfoExtensions.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using ReflectionHelper;
 
 namespace Postulate.Extensions
 {
@@ -22,27 +23,6 @@ namespace Postulate.Extensions
                 { SaveAction.Update, Access.UpdateOnly }
             };
             return HasColumnAccess(provider, map[action]);
-        }
-
-        public static bool HasAttribute<TAttribute>(this ICustomAttributeProvider provider, out TAttribute attribute, Func<TAttribute, bool> predicate = null) where TAttribute : Attribute
-        {
-            attribute = null;            
-            var attrs = provider.GetCustomAttributes(typeof(TAttribute), true).OfType<TAttribute>();
-
-            if (predicate != null) attrs = attrs.Where(a => predicate.Invoke(a));
-
-            if (attrs.Any())
-            {
-                attribute = attrs.First() as TAttribute;
-                return true;
-            }
-            return false;
-        }
-
-        public static bool HasAttribute<TAttribute>(this ICustomAttributeProvider provider, Func<TAttribute, bool> predicate = null) where TAttribute : Attribute
-        {
-            TAttribute attr;
-            return HasAttribute(provider, out attr, predicate);
         }
     }
 }
