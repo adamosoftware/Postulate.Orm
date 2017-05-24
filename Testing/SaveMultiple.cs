37d85955c73f4e4a883b06b1e16b103c26cd51e7
Adam O'Neil <adamosoftware@gmail.com>
2017-05-24 14:44:08 -0400

    SaveMultiple test

---
+++ Testing/SaveMultiple.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testing.Models;
+
+namespace Testing
+{
+    [TestClass]
+    public class SaveMultiple
+    {
+        [TestMethod]
+        public void TestSaveAsync()
+        {
+            var tableBItems = new TableB[]
+            {
+                new TableB() { Description = "whatever", OrganizationId = 1 },
+                new TableB() { Description = "this thing", OrganizationId = 1 },
+                new TableB() { Description = "circumscribed", OrganizationId = 1 },
+                new TableB() { Description = "least confident", OrganizationId = 1 }
+            };
+
+            var db = new PostulateDb();
+            db.SaveAsync(tableBItems);
+        }
+    }
+}
