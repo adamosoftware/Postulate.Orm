55bc0dcd7ac009dd0956084562558ecfeeb6b180
Adam O'Neil <adamosoftware@gmail.com>
2017-06-06 16:07:26 -0400

    added Not implemented comment to TrackDeletions attribute

--- PostulateV1/Attributes/TrackDeletionsAttribute.cs
+++ PostulateV1/Attributes/TrackDeletionsAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace Postulate.Orm.Attributes
 {
+    /// <summary>
+    /// Not implemented
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class TrackDeletionsAttribute : Attribute
     {
