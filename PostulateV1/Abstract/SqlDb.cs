056cfb667af7216d1c99b02e2bc705449d359f0e
Adam O'Neil <adamosoftware@gmail.com>
2017-06-07 10:29:29 -0400

    refactored delete methods to separate file

--- PostulateV1/Abstract/SqlDb.cs
+++ PostulateV1/Abstract/SqlDb.cs
@@ -125,109 +125,6 @@ namespace Postulate.Orm.Abstract
             return FindInner(connection, row);
         }
 
-        public void DeleteOne<TRecord>(IDbConnection connection, TRecord record) where TRecord : Record<TKey>
-        {
-            string message;
-            if (record.AllowDelete(connection, UserName, out message))
-            {                                               
-                if (TrackDeletions<TRecord>())
-                {
-                    using (var txn = GetTransaction(connection))
-                    {
-                        try
-                        {
-                            OnCaptureDeletion(connection, record, txn);
-                            ExecuteDeleteOne<TRecord>(connection, record.Id, txn);
-                            txn.Commit();
-                        }
-                        catch
-                        {
-                            txn.Rollback();
-                            throw;
-                        }
-                    }
-                }
-                else
-                {
-                    ExecuteDeleteOne<TRecord>(connection, record.Id);
-                }
-                
-                record.AfterDelete(connection);
-            }
-            else
-            {
-                throw new PermissionDeniedException(message);
-            }
-        }
-
-        public TKey RestoreOne<TRecord>(IDbConnection connection, TKey id) where TRecord : Record<TKey>
-        {
-            var record = BeginRestore<TRecord>(connection, id);            
-
-            using (var txn = GetTransaction(connection))
-            {
-                try
-                {
-                    TKey newId = ExecuteInsert(connection, record, txn);
-                    CompleteRestore<TRecord>(connection, id, txn);
-                    txn.Commit();
-                    return newId;
-                }
-                catch
-                {
-                    txn.Rollback();
-                    throw;
-                }
-            }
-        }
-
-        public void DeleteOne<TRecord>(IDbConnection connection, TKey id) where TRecord : Record<TKey>
-        {
-            TRecord record = Find<TRecord>(connection, id);
-            if (record != null) DeleteOne(connection, record);
-        }
-
-        public void DeleteOneWhere<TRecord>(IDbConnection connection, string criteria, object parameters) where TRecord : Record<TKey>
-        {
-            TRecord record = FindWhere<TRecord>(connection, criteria, parameters);
-            if (record != null) DeleteOne<TRecord>(connection, record.Id);
-        }
-
-        public int DeleteAllWhere<TRecord>(IDbConnection connection, string criteria, object parameters) where TRecord : Record<TKey>
-        {
-            string cmd = $"DELETE {GetTableName<TRecord>()} WHERE {criteria}";
-
-            if (TrackDeletions<TRecord>())
-            {                
-                var records = connection.Query<TRecord>($"SELECT * FROM {GetTableName<TRecord>()} WHERE {criteria}", parameters);
-
-                using (var txn = GetTransaction(connection))
-                {
-                    try
-                    {
-                        foreach (var record in records) OnCaptureDeletion(connection, record, txn);
-                        int result = connection.Execute(cmd, parameters, txn);
-                        txn.Commit();
-                        return result;
-                    }
-                    catch
-                    {
-                        txn.Rollback();
-                        throw;
-                    }
-                }
-            }
-            else
-            {                
-                return connection.Execute(cmd, parameters);
-            }
-        }
-
-        private bool TrackDeletions<TRecord>() where TRecord : Record<TKey>
-        {
-            return typeof(TRecord).HasAttribute<TrackDeletionsAttribute>();
-        }
-
         public TRecord FindUserProfile<TRecord>(IDbConnection connection) where TRecord : Record<TKey>, IUserProfile
         {
             return ExecuteFindWhere<TRecord>(connection, "[UserName]=@name", new { name = UserName });
@@ -337,12 +234,6 @@ namespace Postulate.Orm.Abstract
         {
             string cmd = GetFindStatementBase<TRecord>() + $" WHERE {criteria}";
             return connection.QuerySingleOrDefault<TRecord>(cmd, parameters);
-        }
-
-        private void ExecuteDeleteOne<TRecord>(IDbConnection connection, TKey id, IDbTransaction txn = null) where TRecord : Record<TKey>
-        {
-            string cmd = GetCommand<TRecord>(_deleteCommands, () => GetDeleteStatement<TRecord>());
-            connection.Execute(cmd, new { id = id }, txn);
         }
 
         private string GetCommand<TRecord>(Dictionary<string, string> dictionary, Func<string> commandBuilder)
