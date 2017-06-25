e1f662fbc417759ed82de1783ddeb7bd29e654cc
Adam O'Neil <adamosoftware@gmail.com>
2017-06-25 14:08:49 -0400

    version 0.9.169 SeedData class

---
+++ PostulateV1/Abstract/SeedData.cs
@@ -0,0 +1,23 @@
+﻿using Postulate.Orm.Extensions;
+using System.Collections.Generic;
+
+namespace Postulate.Orm.Abstract
+{
+    public abstract class SeedData<TRecord, TKey> where TRecord : Record<TKey>
+    {
+        public abstract string ExistsCriteria { get; }
+        public abstract IEnumerable<TRecord> Records { get; }
+
+        public void Generate(SqlDb<TKey> db)
+        {
+            using (var cn = db.GetConnection())
+            {
+                cn.Open();
+                foreach (var record in Records)
+                {
+                    if (!cn.Exists(ExistsCriteria, record)) db.Save(record);
+                }
+            }
+        }
+    }
+}
