e70088c1e4526fdf64d9b0153b82431c8f01a42c
Adam O'Neil <adamosoftware@gmail.com>
2017-07-07 13:36:08 -0400

    version 0.9.184 FindOrCreate

---
+++ PostulateV1/Abstract/SqlDb_FindOrCreate.cs
@@ -0,0 +1,11 @@
+﻿namespace Postulate.Orm.Abstract
+{
+    public abstract partial class SqlDb<TKey>
+    {
+        public TRecord FindOrCreate<TRecord>(string criteria, TRecord instance) where TRecord : Record<TKey>
+        {
+            var result = FindWhere<TRecord>(criteria, instance);
+            return result ?? instance;
+        }
+    }
+}
