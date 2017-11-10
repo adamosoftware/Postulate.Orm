102d874b8143d81576a0f7b747837086d3ba2909
Adam O'Neil <adamosoftware@gmail.com>
2017-11-10 09:33:49 -0500

    updated more assembly info

--- Merge.MySql/Properties/AssemblyInfo.cs
+++ Merge.MySql/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Merge.MySql")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Postulate.Orm.MySql")]
+[assembly: AssemblyDescription("Postulate implementation for MySql")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Merge.MySql")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Adam O'Neil Software")]
+[assembly: AssemblyProduct("Postulate.Orm.MySql")]
+[assembly: AssemblyCopyright("Copyright © Adam O'Neil Software 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Merge.SqlServer/Properties/AssemblyInfo.cs
+++ Merge.SqlServer/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Merge.SqlServer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Postulate.Orm.SqlServer")]
+[assembly: AssemblyDescription("Postulate implementation for SQL Server")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Merge.SqlServer")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Adam O'Neil Software")]
+[assembly: AssemblyProduct("Postulate.Orm.SqlServer")]
+[assembly: AssemblyCopyright("Copyright © Adam O'Neil 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
