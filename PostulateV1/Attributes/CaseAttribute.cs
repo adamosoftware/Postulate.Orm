a750051b7e689d45df91db7f15fba899659e34fc
Adam O'Neil <adamosoftware@gmail.com>
2017-11-18 17:46:14 -0500

    added WhereAttribute comment

--- PostulateV1/Attributes/CaseAttribute.cs
+++ PostulateV1/Attributes/CaseAttribute.cs
@@ -4,6 +4,7 @@ namespace Postulate.Orm.Attributes
 {
     /// <summary>
     /// Used with classes based on <see cref="Abstract.Query{TResult}"/> to allow you to map specific criteria expressions with specific property values.
+    /// Cannot be used with <see cref="WhereAttribute"/>.
     /// </summary>
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
     public class CaseAttribute : Attribute
