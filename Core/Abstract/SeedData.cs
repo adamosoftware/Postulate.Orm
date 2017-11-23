21e4e3a81ebde2706bf5a644f8da72b8217bb554
Adam O'Neil <adamosoftware@gmail.com>
2017-11-23 14:03:53 -0500

    added SeedData property comments and removed unnecessary using

--- Core/Abstract/SeedData.cs
+++ Core/Abstract/SeedData.cs
@@ -1,5 +1,4 @@
 ﻿using Dapper;
-using Postulate.Orm.Extensions;
 using System.Collections.Generic;
 using System.Data;
 
@@ -7,7 +6,14 @@ namespace Postulate.Orm.Abstract
 {
     public abstract class SeedData<TRecord, TKey> where TRecord : Record<TKey>, new()
     {
+        /// <summary>
+        /// FROM and WHERE clause used to determine whether a seed row exists or not
+        /// </summary>
         public abstract string ExistsCriteria { get; }
+
+        /// <summary>
+        /// Array of records to generate
+        /// </summary>
         public abstract IEnumerable<TRecord> Records { get; }
 
         public void Generate(IDbConnection connection, SqlDb<TKey> db)
@@ -16,7 +22,7 @@ namespace Postulate.Orm.Abstract
             {
                 var existingRecord = connection.QuerySingleOrDefault<TRecord>($"SELECT * FROM {ExistsCriteria}", record);
                 // this will cause the existing seed record to be updated instead of inserted
-                if (existingRecord != null) record.Id = existingRecord.Id;                
+                if (existingRecord != null) record.Id = existingRecord.Id;
                 db.Save(connection, record);
             }
         }
