ce6dd79ecdc43fa8a9a46e25b2a7651efda55b6c
Adam O'Neil <adamosoftware@gmail.com>
2017-12-17 12:26:38 -0500

    started on EnumTable attribute

---
+++ Core/Attributes/EnumTableAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Postulate.Orm.Attributes
+{
+    [AttributeUsage(AttributeTargets.Enum, AllowMultiple = false)]
+    public class EnumTableAttribute : Attribute
+    {
+        private readonly string _tableName;
+
+        public EnumTableAttribute(string tableName)
+        {
+            _tableName = tableName;
+        }
+    }
+}
---
+++ Core/Merge/Actions/EnumTable.cs
@@ -0,0 +1,26 @@
+﻿using Postulate.Orm.Abstract;
+using Postulate.Orm.Attributes;
+using ReflectionHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Postulate.Orm.Merge.Actions
+{
+    public class EnumTable : MergeAction
+    {
+        private readonly Type _enumType;
+        private readonly EnumTableAttribute _attribute;
+
+        public EnumTable(SqlSyntax syntax, Type enumType) : base(syntax, ObjectType.Table, ActionType.Create, $"Enum table {enumType.Name}")
+        {
+            _enumType = enumType;
+            _attribute = enumType.GetAttribute<EnumTableAttribute>() ?? throw new Exception($"Enum type {enumType.Name} is missing an [EnumTable] attribute");
+        }
+
+        public override IEnumerable<string> SqlCommands(IDbConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
