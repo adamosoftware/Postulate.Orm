784935467575a8f015deff52f772ee0af8406807
Adam O'Neil <adamosoftware@gmail.com>
2017-12-23 20:06:26 -0500

    package updates (Dapper 1.50.4), and started on SqlCe implementation

---
+++ Orm.SqlCe/SqlCeDb.cs
@@ -0,0 +1,67 @@
+﻿using Postulate.Orm.Abstract;
+using Postulate.Orm.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlServerCe;
+
+namespace Postulate.Orm.SqlCe
+{
+    public class SqlCeDb : SqlDb<int>
+    {
+        public SqlCeDb(string connectionString) : base(connectionString, new SqlCeSyntax(), connectionSource: ConnectionSource.Literal)
+        {
+        }        
+
+        public override void CreateIfNotExists(Action<IDbConnection, bool> seedAction = null)
+        {
+            var engine = new SqlCeEngine(ConnectionString);
+            engine.CreateDatabase();
+        }
+
+        public override IDbConnection GetConnection()
+        {
+            return new SqlCeConnection(ConnectionString);
+        }
+
+        public override int GetRecordNextVersion<TRecord>(IDbConnection connection, int id)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override int GetRecordNextVersion<TRecord>(int id)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override IDbTransaction GetTransaction(IDbConnection connection)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override IEnumerable<ChangeHistory<int>> QueryChangeHistory<TRecord>(IDbConnection connection, int id, int timeZoneOffset = 0)
+        {
+            throw new NotSupportedException();
+        }
+
+        protected override TRecord BeginRestore<TRecord>(IDbConnection connection, int id)
+        {
+            throw new NotSupportedException();
+        }
+
+        protected override void CompleteRestore<TRecord>(IDbConnection connection, int id, IDbTransaction transaction)
+        {
+            throw new NotSupportedException();
+        }
+
+        protected override void OnCaptureChanges<TRecord>(IDbConnection connection, int id, IEnumerable<PropertyChange> changes)
+        {
+            throw new NotSupportedException();
+        }
+
+        protected override void OnCaptureDeletion<TRecord>(IDbConnection connection, TRecord record, IDbTransaction transasction)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ Orm.SqlCe/SqlCeSyntax.cs
@@ -0,0 +1,250 @@
+﻿using Postulate.Orm.Abstract;
+using Postulate.Orm.Exceptions;
+using Postulate.Orm.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+namespace Postulate.Orm.SqlCe
+{
+    public class SqlCeSyntax : SqlSyntax
+    {
+        public override string CommentPrefix => throw new NotImplementedException();
+
+        public override string CommandSeparator => throw new NotImplementedException();
+
+        public override string TableExistsQuery => throw new NotImplementedException();
+
+        public override string ColumnExistsQuery => throw new NotImplementedException();
+
+        public override string IndexExistsQuery => throw new NotImplementedException();
+
+        public override string SchemaColumnQuery => throw new NotImplementedException();
+
+        public override string ApplyDelimiter(string objectName)
+        {
+            return $"`{objectName}`";
+        }
+
+        public override string ApplyPaging(string sql, int pageNumber, int rowsPerPage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string CheckEnumValueExistsStatement(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ColumnAddStatement(TableInfo tableInfo, PropertyInfo propertyInfo, bool forceNull = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ColumnAlterStatement(TableInfo tableInfo, PropertyInfo propertyInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ColumnDropStatement(ColumnInfo columnInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ColumnExistsParameters(PropertyInfo propertyInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string CreateColumnIndexStatement(PropertyInfo propertyInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string CreateEnumTableStatement(Type enumType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string CreateSchemaStatement(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] CreateTableMembers(Type type, IEnumerable<string> addedColumns, IEnumerable<string> modifiedColumns, IEnumerable<string> deletedColumns)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool FindObjectId(IDbConnection connection, TableInfo tableInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ForeignKeyAddStatement(PropertyInfo propertyInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ForeignKeyAddStatement(ForeignKeyInfo foreignKeyInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ForeignKeyDropStatement(ForeignKeyInfo foreignKeyInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetColumnDefault(PropertyInfo propertyInfo, bool forCreateTable = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetColumnSyntax(PropertyInfo propertyInfo, bool forceNull = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetColumnType(PropertyInfo propertyInfo, bool forceNull = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetConstraintBaseName(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetCopyStatement<TRecord, TKey>(IEnumerable<string> paramColumns, IEnumerable<string> columns)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<ForeignKeyInfo> GetDependentForeignKeys(IDbConnection connection, TableInfo tableInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<KeyColumnInfo> GetKeyColumns(IDbConnection connection, Func<KeyColumnInfo, bool> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ILookup<int, ColumnInfo> GetSchemaColumns(IDbConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<TableInfo> GetSchemaTables(IDbConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetScriptFromSaveException(SaveException exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override TableInfo GetTableInfoFromType(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetTableName(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string InsertEnumValueStatement(string tableName, string name, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsColumnInPrimaryKey(IDbConnection connection, ColumnInfo columnInfo, out bool clustered, out string constraintName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsTableEmpty(IDbConnection connection, Type t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Dictionary<Type, string> KeyTypeMap(bool withDefaults = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string PrimaryKeyAddStatement(TableInfo affectedTable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string PrimaryKeyDropStatement(TableInfo affectedTable, string pkName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object SchemaColumnParameters(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool SchemaExists(IDbConnection connection, string schemaName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string SqlDataType(PropertyInfo propertyInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Dictionary<Type, string> SupportedTypes(string length = null, byte precision = 0, byte scale = 0)
+        {
+            return new Dictionary<Type, string>()
+            {
+                { typeof(bool), "bit" },
+                { typeof(Byte), "tinyint" },
+                { typeof(Byte[]), $"varbinary({length})" },
+                { typeof(char), $"char({length})" },
+                { typeof(DateTime), "datetime" },
+                { typeof(decimal), "numeric" },
+                { typeof(double), "float" },
+                { typeof(short), "smallint" },
+                { typeof(int), "int" },
+                { typeof(long), "bigint" },
+                { typeof(Single), "real" },
+                { typeof(string), (length.Equals("max")) ? "ntext" : $"nvarchar({length})" }
+            };
+        }
+
+        public override string TableCreateStatement(Type type, IEnumerable<string> addedColumns, IEnumerable<string> modifiedColumns, IEnumerable<string> deletedColumns)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string TableDropStatement(TableInfo tableInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object TableExistsParameters(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string UpdateColumnWithExpressionStatement(TableInfo tableInfo, PropertyInfo propertyInfo, string expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetExcludeSchemas(IDbConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
