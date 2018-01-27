43e036521c60de5c440f6e6286815d337982a98d
Adam O'Neil <adamosoftware@gmail.com>
2018-01-27 17:52:22 -0500

    code-maid cleanup

--- Core/Query.cs
+++ Core/Query.cs
@@ -1,7 +1,8 @@
 ﻿using Dapper;
+using Postulate.Orm.Attributes;
+using Postulate.Orm.Extensions;
 using Postulate.Orm.Interfaces;
 using Postulate.Orm.Models;
-using Postulate.Orm.Attributes;
 using ReflectionHelper;
 using System;
 using System.Collections.Generic;
@@ -9,169 +10,168 @@ using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Postulate.Orm.Extensions;
 
 namespace Postulate.Orm
 {
-    /// <summary>
-    /// Provides strong-typed access to inline SQL queries, with dynamic criteria and tracing capability
-    /// </summary>
-    /// <typeparam name="TResult">Type with properties that map to columns returned by the query</typeparam>
-    public class Query<TResult>
-    {
-        private readonly string _sql;      
-        private readonly ISqlDb _db;
-        private string _resolvedSql;
+	/// <summary>
+	/// Provides strong-typed access to inline SQL queries, with dynamic criteria and tracing capability
+	/// </summary>
+	/// <typeparam name="TResult">Type with properties that map to columns returned by the query</typeparam>
+	public class Query<TResult>
+	{
+		private readonly string _sql;
+		private readonly ISqlDb _db;
+		private string _resolvedSql;
 
-        /// <summary>
-        /// Set this to receive metrics about the last query executed, including the user name, full SQL, parameter info, and duration
-        /// </summary>
-        public Action<IDbConnection, QueryTrace> TraceCallback { get; set; }
+		/// <summary>
+		/// Set this to receive metrics about the last query executed, including the user name, full SQL, parameter info, and duration
+		/// </summary>
+		public Action<IDbConnection, QueryTrace> TraceCallback { get; set; }
 
-        public Query(string sql, ISqlDb db)
-        {
-            _sql = sql;
-            _db = db;
-        }
+		public Query(string sql, ISqlDb db)
+		{
+			_sql = sql;
+			_db = db;
+		}
 
-        protected ISqlDb Db { get { return _db; } }
+		protected ISqlDb Db { get { return _db; } }
 
-        public int RowsPerPage { get; set; } = 50;
+		public int RowsPerPage { get; set; } = 50;
 
-        public string Sql { get { return _sql; } }
+		public string Sql { get { return _sql; } }
 
-        public string ResolvedSql { get { return _resolvedSql; } }
+		public string ResolvedSql { get { return _resolvedSql; } }
 
-        public int CommandTimeout { get; set; } = 30;
+		public int CommandTimeout { get; set; } = 30;
 
-        public CommandType CommandType { get; set; } = CommandType.Text;
+		public CommandType CommandType { get; set; } = CommandType.Text;
 
-        /// <summary>
-        /// Lets you specify where in the application a query is called so that trace information is potentially more useful
-        /// </summary>
-        public string TraceContext { get; set; }
+		/// <summary>
+		/// Lets you specify where in the application a query is called so that trace information is potentially more useful
+		/// </summary>
+		public string TraceContext { get; set; }
 
-        public IEnumerable<TResult> Execute(int pageNumber = 0)
-        {
-            using (var cn = _db.GetConnection())
-            {
-                cn.Open();
-                return ExecuteInner(cn, pageNumber);
-            }
-        }
+		public IEnumerable<TResult> Execute(int pageNumber = 0)
+		{
+			using (var cn = _db.GetConnection())
+			{
+				cn.Open();
+				return ExecuteInner(cn, pageNumber);
+			}
+		}
 
-        public IEnumerable<TResult> Execute(IDbConnection connection, int pageNumber = 0)
-        {
-            return ExecuteInner(connection, pageNumber);
-        }
+		public IEnumerable<TResult> Execute(IDbConnection connection, int pageNumber = 0)
+		{
+			return ExecuteInner(connection, pageNumber);
+		}
 
-        public TResult ExecuteSingle()
-        {
-            using (var cn = _db.GetConnection())
-            {
-                cn.Open();
-                return ExecuteSingle(cn);
-            }
-        }
+		public TResult ExecuteSingle()
+		{
+			using (var cn = _db.GetConnection())
+			{
+				cn.Open();
+				return ExecuteSingle(cn);
+			}
+		}
 
-        public TResult ExecuteSingle(IDbConnection connection)
-        {
+		public TResult ExecuteSingle(IDbConnection connection)
+		{
 			_resolvedSql = ResolveQuery(_sql, this, 0, out List<QueryTrace.Parameter> parameters, out DynamicParameters queryParams);
 
 			Stopwatch sw = Stopwatch.StartNew();
-            TResult result = connection.QueryFirstOrDefault<TResult>(_resolvedSql, queryParams);
-            sw.Stop();
+			TResult result = connection.QueryFirstOrDefault<TResult>(_resolvedSql, queryParams);
+			sw.Stop();
 
-            InvokeTraceCallback(connection, parameters, sw);
+			InvokeTraceCallback(connection, parameters, sw);
 
-            return result;
-        }
+			return result;
+		}
 
-        private void InvokeTraceCallback(IDbConnection connection, List<QueryTrace.Parameter> parameters, Stopwatch sw)
-        {
-            TraceCallback?.Invoke(connection, new QueryTrace(GetType().FullName, _db.UserName, _resolvedSql, parameters, sw.ElapsedMilliseconds, TraceContext));
-        }
+		private void InvokeTraceCallback(IDbConnection connection, List<QueryTrace.Parameter> parameters, Stopwatch sw)
+		{
+			TraceCallback?.Invoke(connection, new QueryTrace(GetType().FullName, _db.UserName, _resolvedSql, parameters, sw.ElapsedMilliseconds, TraceContext));
+		}
 
-        public async Task<TResult> ExecuteSingleAsync()
-        {
-            using (var cn = _db.GetConnection())
-            {
-                cn.Open();
-                return await ExecuteSingleAsync(cn);
-            }
-        }
+		public async Task<TResult> ExecuteSingleAsync()
+		{
+			using (var cn = _db.GetConnection())
+			{
+				cn.Open();
+				return await ExecuteSingleAsync(cn);
+			}
+		}
 
-        public async Task<TResult> ExecuteSingleAsync(IDbConnection connection)
-        {
+		public async Task<TResult> ExecuteSingleAsync(IDbConnection connection)
+		{
 			_resolvedSql = ResolveQuery(_sql, this, -1, out List<QueryTrace.Parameter> parameters, out DynamicParameters queryParams);
 
 			Stopwatch sw = Stopwatch.StartNew();
-            TResult result = await connection.QueryFirstOrDefaultAsync<TResult>(_resolvedSql, queryParams);
-            sw.Stop();
+			TResult result = await connection.QueryFirstOrDefaultAsync<TResult>(_resolvedSql, queryParams);
+			sw.Stop();
 
-            InvokeTraceCallback(connection, parameters, sw);
+			InvokeTraceCallback(connection, parameters, sw);
 
-            return result;
-        }
+			return result;
+		}
 
-        public async Task<IEnumerable<TResult>> ExecuteAsync(int pageNumber = 0)
-        {
-            using (var cn = _db.GetConnection())
-            {
-                cn.Open();
-                return await ExecuteInnerAsync(cn, pageNumber);
-            }
-        }
+		public async Task<IEnumerable<TResult>> ExecuteAsync(int pageNumber = 0)
+		{
+			using (var cn = _db.GetConnection())
+			{
+				cn.Open();
+				return await ExecuteInnerAsync(cn, pageNumber);
+			}
+		}
 
-        public async Task<IEnumerable<TResult>> ExecuteAsync(IDbConnection connection, int pageNumber = 0)
-        {
-            return await ExecuteInnerAsync(connection, pageNumber);
-        }
+		public async Task<IEnumerable<TResult>> ExecuteAsync(IDbConnection connection, int pageNumber = 0)
+		{
+			return await ExecuteInnerAsync(connection, pageNumber);
+		}
 
-        private IEnumerable<TResult> ExecuteInner(IDbConnection connection, int pageNumber = 0)
-        {
-            IEnumerable<TResult> results = null;
-
-			_resolvedSql = ResolveQuery(_sql, this, pageNumber, out List<QueryTrace.Parameter> parameters, out DynamicParameters queryParams);
-
-			Stopwatch sw = Stopwatch.StartNew();
-            results = connection.Query<TResult>(_resolvedSql, queryParams);
-            sw.Stop();
-
-            InvokeTraceCallback(connection, parameters, sw);
-
-            return results;
-        }
-
-        private async Task<IEnumerable<TResult>> ExecuteInnerAsync(IDbConnection connection, int pageNumber = 0)
-        {
-            IEnumerable<TResult> results = null;
+		private IEnumerable<TResult> ExecuteInner(IDbConnection connection, int pageNumber = 0)
+		{
+			IEnumerable<TResult> results = null;
 
 			_resolvedSql = ResolveQuery(_sql, this, pageNumber, out List<QueryTrace.Parameter> parameters, out DynamicParameters queryParams);
 
 			Stopwatch sw = Stopwatch.StartNew();
-            results = await connection.QueryAsync<TResult>(_resolvedSql, queryParams);
-            sw.Stop();
+			results = connection.Query<TResult>(_resolvedSql, queryParams);
+			sw.Stop();
 
-            InvokeTraceCallback(connection, parameters, sw);
+			InvokeTraceCallback(connection, parameters, sw);
 
-            return results;
-        }
+			return results;
+		}
 
-        private static string ResolveQuery(string sql, Query<TResult> query, int pageNumber, out List<QueryTrace.Parameter> parameters, out DynamicParameters queryParams)
-        {
-            string result = sql;
-            List<string> terms = new List<string>();
-            parameters = new List<QueryTrace.Parameter>();
+		private async Task<IEnumerable<TResult>> ExecuteInnerAsync(IDbConnection connection, int pageNumber = 0)
+		{
+			IEnumerable<TResult> results = null;
 
-            // this gets the param names within the query based on words with leading '@'
-            var builtInParams = sql.GetParameterNames(true).Select(p => p.ToLower());
+			_resolvedSql = ResolveQuery(_sql, this, pageNumber, out List<QueryTrace.Parameter> parameters, out DynamicParameters queryParams);
 
-            // these are properties of the Query base type that we ignore because they are never part of WHERE clause (things like CommandType and CommandTimeout)
-            var baseProps = query.GetType().BaseType.GetProperties().Select(pi => pi.Name);
+			Stopwatch sw = Stopwatch.StartNew();
+			results = await connection.QueryAsync<TResult>(_resolvedSql, queryParams);
+			sw.Stop();
 
-            // these are the properties of the Query that are explicitly defined and may impact the WHERE clause
-            var queryProps = query.GetType().GetProperties().Where(pi => !baseProps.Contains(pi.Name));
+			InvokeTraceCallback(connection, parameters, sw);
+
+			return results;
+		}
+
+		private static string ResolveQuery(string sql, Query<TResult> query, int pageNumber, out List<QueryTrace.Parameter> parameters, out DynamicParameters queryParams)
+		{
+			string result = sql;
+			List<string> terms = new List<string>();
+			parameters = new List<QueryTrace.Parameter>();
+
+			// this gets the param names within the query based on words with leading '@'
+			var builtInParams = sql.GetParameterNames(true).Select(p => p.ToLower());
+
+			// these are properties of the Query base type that we ignore because they are never part of WHERE clause (things like CommandType and CommandTimeout)
+			var baseProps = query.GetType().BaseType.GetProperties().Select(pi => pi.Name);
+
+			// these are the properties of the Query that are explicitly defined and may impact the WHERE clause
+			var queryProps = query.GetType().GetProperties().Where(pi => !baseProps.Contains(pi.Name));
 
 			queryParams = new DynamicParameters();
 			foreach (var prop in queryProps)
@@ -180,60 +180,60 @@ namespace Postulate.Orm
 				if (value != null) queryParams.Add(prop.Name, value);
 			}
 
-            Dictionary<string, string> whereBuilder = new Dictionary<string, string>()
-            {
-                { InternalStringExtensions.WhereToken, "WHERE" }, // query has no where clause, so it needs the word WHERE inserted
+			Dictionary<string, string> whereBuilder = new Dictionary<string, string>()
+			{
+				{ InternalStringExtensions.WhereToken, "WHERE" }, // query has no where clause, so it needs the word WHERE inserted
                 { InternalStringExtensions.AndWhereToken, "AND" } // query already contains a WHERE clause, we're just adding to it
             };
-            string token;
-            if (result.ContainsAny(new string[] { InternalStringExtensions.WhereToken, InternalStringExtensions.AndWhereToken }, out token))
-            {
-                bool anyCriteria = false;                                
-                               
-                // loop through this query's properties, but ignore base properties (like ResolvedSql and TraceCallback) since they are never part of WHERE clause                
-                foreach (var pi in queryProps)
-                {
-                    object value = pi.GetValue(query);
-                    if (value != null)
-                    {
-                        parameters.Add(new QueryTrace.Parameter() { Name = pi.Name, Value = value });
+			string token;
+			if (result.ContainsAny(new string[] { InternalStringExtensions.WhereToken, InternalStringExtensions.AndWhereToken }, out token))
+			{
+				bool anyCriteria = false;
 
-                        // built-in params are not part of the WHERE clause, so they are excluded from added terms
-                        if (!builtInParams.Contains(pi.Name.ToLower()))
-                        {
-                            anyCriteria = true;
+				// loop through this query's properties, but ignore base properties (like ResolvedSql and TraceCallback) since they are never part of WHERE clause
+				foreach (var pi in queryProps)
+				{
+					object value = pi.GetValue(query);
+					if (value != null)
+					{
+						parameters.Add(new QueryTrace.Parameter() { Name = pi.Name, Value = value });
 
-                            var cases = pi.GetCustomAttributes(typeof(CaseAttribute), false).OfType<CaseAttribute>();
-                            var selectedCase = cases?.FirstOrDefault(c => c.Value.Equals(value));
-                            if (selectedCase != null)
-                            {
-                                terms.Add(selectedCase.Expression);
-                            }
-                            else
-                            {
-                                WhereAttribute whereAttr = pi.GetAttribute<WhereAttribute>();
-                                string expression = (whereAttr != null) ? whereAttr.Expression : $"{query._db.Syntax.ApplyDelimiter(pi.Name)}=@{pi.Name}";
-                                terms.Add(expression);                                
-                            }
-                        }
-                    }
-                }
-                result = result.Replace(token, (anyCriteria) ? $"{whereBuilder[token]} {string.Join(" AND ", terms)}" : string.Empty);
-            }
-            
-            // need to add built in params explicitly to outgoing param list for the benefit of QueryTrace
-            foreach (var parameter in builtInParams)
-            {
-                var paramProperty = queryProps.Single(pi => pi.Name.ToLower().Equals(parameter));
-                parameters.Add(new QueryTrace.Parameter() { Name = parameter, Value = paramProperty.GetValue(query) });
-            }
+						// built-in params are not part of the WHERE clause, so they are excluded from added terms
+						if (!builtInParams.Contains(pi.Name.ToLower()))
+						{
+							anyCriteria = true;
 
-            if (pageNumber > 0)
-            {
-                result = query.Db.Syntax.ApplyPaging(result, pageNumber, query.RowsPerPage);
-            }
+							var cases = pi.GetCustomAttributes(typeof(CaseAttribute), false).OfType<CaseAttribute>();
+							var selectedCase = cases?.FirstOrDefault(c => c.Value.Equals(value));
+							if (selectedCase != null)
+							{
+								terms.Add(selectedCase.Expression);
+							}
+							else
+							{
+								WhereAttribute whereAttr = pi.GetAttribute<WhereAttribute>();
+								string expression = (whereAttr != null) ? whereAttr.Expression : $"{query._db.Syntax.ApplyDelimiter(pi.Name)}=@{pi.Name}";
+								terms.Add(expression);
+							}
+						}
+					}
+				}
+				result = result.Replace(token, (anyCriteria) ? $"{whereBuilder[token]} {string.Join(" AND ", terms)}" : string.Empty);
+			}
 
-            return result;
-        }
-    }
+			// need to add built in params explicitly to outgoing param list for the benefit of QueryTrace
+			foreach (var parameter in builtInParams)
+			{
+				var paramProperty = queryProps.Single(pi => pi.Name.ToLower().Equals(parameter));
+				parameters.Add(new QueryTrace.Parameter() { Name = parameter, Value = paramProperty.GetValue(query) });
+			}
+
+			if (pageNumber > 0)
+			{
+				result = query.Db.Syntax.ApplyPaging(result, pageNumber, query.RowsPerPage);
+			}
+
+			return result;
+		}
+	}
 }
